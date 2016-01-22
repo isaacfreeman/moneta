b0d0cd6f2001a54d95d06d3c156bee77502cd6f2
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-01-22 16:38:08 +1300

    - adds moneta_body_class helper to help namespace styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Helper methods that will be used widely across the application
 module ApplicationHelper
+  def moneta_body_class(options = {})
+    extra_body_classes_symbol = options[:extra_body_classes_symbol] || :extra_body_classes
+    qualified_controller_name = controller.controller_path.gsub('/','-')
+    basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
+    if content_for?(extra_body_classes_symbol)
+      [basic_body_class, content_for(extra_body_classes_symbol)].join(' ')
+    else
+      basic_body_class
+    end
+  end
 end
